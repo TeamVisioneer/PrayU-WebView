fb3b99678f7014a381b96636a8636d10459b2ebe
Junbeom Park <ephesi4ns623@gmail.com>
2024-09-02 14:09:30 +0900

    feat: enable back key

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -4,25 +4,42 @@ import 'package:url_launcher/url_launcher.dart';
 
 final homeUrl = Uri.parse('https://prayu.vercel.app/');
 
-class HomeScreen extends StatelessWidget {
-  final WebViewController controller;
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
 
-  HomeScreen({super.key})
-      : controller = WebViewController()
-    ..setJavaScriptMode(JavaScriptMode.unrestricted)
-    ..setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36')
-    ..loadRequest(homeUrl);
+class _HomeScreenState extends State<HomeScreen> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36')
+      ..loadRequest(homeUrl);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: const Text('PrayU'),
-        centerTitle: true,
-      ),
-      body: WebViewWidget(
-        controller: controller,
+    return WillPopScope(
+      onWillPop: () async {
+        if (await _controller.canGoBack()) {
+          _controller.goBack();
+          return false; // 웹뷰에서 뒤로가기를 수행하고, 앱의 뒤로가기 동작을 막음
+        }
+        return true; // 앱의 기본 뒤로가기 동작을 수행
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: const Text('PrayU'),
+          centerTitle: true,
+        ),
+        body: WebViewWidget(
+          controller: _controller,
+        ),
       ),
     );
   }
