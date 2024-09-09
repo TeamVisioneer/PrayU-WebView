3540480cff43aa613c503d9bdc14cdec414ee10e
Junbeom Park <ephesi4ns623@gmail.com>
2024-09-09 19:09:02 +0900

    fix: 카카오 인앱 동의하기 파싱

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -3,7 +3,7 @@ import 'package:webview_flutter/webview_flutter.dart';
 import 'package:flutter/services.dart';
 
 // final homeUrl = Uri.parse('http://172.20.10.2:5173/');
-final homeUrl = Uri.parse('https://prayu.vercel.app/');
+final homeUrl = Uri.parse('https://prayu-staging.vercel.app/group/5d448dc0-0518-4e3d-997e-6664d18a27f7');
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -25,6 +25,13 @@ class _HomeScreenState extends State<HomeScreen> {
             /* intent://로 시작하는 URL을 가로채어 네이티브 코드로 처리 */
             _launchIntentURL(request.url);
             return NavigationDecision.prevent;
+          } else if (request.url.startsWith('intent:#')) {
+            /* intent:#로 시작하는 URL을 처리하여 새로운 URL로 변환 */
+            String? newUrl = parseKakaoIntentUrl(request.url);
+            if (newUrl != null) {
+              _controller.loadRequest(Uri.parse(newUrl)); // WebView에 새 URL 로드
+            }
+            return NavigationDecision.prevent;
           }
           return NavigationDecision.navigate;
         },
@@ -41,6 +48,41 @@ class _HomeScreenState extends State<HomeScreen> {
     } on PlatformException catch (e) {
       print("Failed to launch intent: '${e.message}'.");
     }
+  }
+
+  String? parseKakaoIntentUrl(String intentUri) {
+    // 'S.browser_fallback_url=' 뒤의 URL을 추출
+    final startIndex = intentUri.indexOf('S.browser_fallback_url=');
+    if (startIndex == -1) return null;
+
+    // 추출한 fallback_url 부분을 디코딩
+    final encodedFallbackUrl = intentUri.substring(startIndex + 'S.browser_fallback_url='.length);
+    final fallbackUrl = Uri.decodeComponent(encodedFallbackUrl);
+
+    // 파싱된 fallback URL에서 쿼리 파라미터 추출
+    Uri parsedUri = Uri.parse(fallbackUrl);
+    String clientId = parsedUri.queryParameters['client_id'] ?? '';
+    String scope = parsedUri.queryParameters['scope'] ?? '';
+    String state = parsedUri.queryParameters['state'] ?? '';
+    String redirectUri = parsedUri.queryParameters['redirect_uri'] ?? '';
+    String responseType = parsedUri.queryParameters['response_type'] ?? '';
+    String authTranId = parsedUri.queryParameters['auth_tran_id'] ?? '';
+    String ka = parsedUri.queryParameters['ka'] ?? '';
+    String isPopup = parsedUri.queryParameters['is_popup'] ?? 'false';
+
+    // 새로운 URL 생성
+    Uri newUri = Uri.https('kauth.kakao.com', '/oauth/authorize', {
+      'client_id': clientId,
+      'scope': scope,
+      'state': state,
+      'redirect_uri': redirectUri,
+      'response_type': responseType,
+      'auth_tran_id': authTranId,
+      'ka': ka,
+      'is_popup': isPopup,
+    });
+
+    return newUri.toString();
   }
 
   @override
