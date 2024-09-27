64c39c780a6d31bcdb7ea3962e2017c7f3093fbc
Junbeom Park <ephesi4ns623@gmail.com>
2024-09-27 17:15:34 +0900

    fix: 백스와이프시 드로워 안내려감 이슈 해결 + 백스와이프 시작 위치 수정

--- lib/models/webview_model.dart
+++ lib/models/webview_model.dart
@@ -1,7 +1,6 @@
 class WebViewModel {
   Uri homeUrl;
-
-  //WebViewModel() : homeUrl = Uri.parse('http://169.254.233.116:5173/');
+  //WebViewModel() : homeUrl = Uri.parse('http://169.254.240.191:5173/');
   //WebViewModel() : homeUrl = Uri.parse('https://prayu-staging.vercel.app');
   WebViewModel() : homeUrl = Uri.parse('https://prayu.site');
 }
