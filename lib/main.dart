51e7e17298851ec404d80e052c89d9a3b3350dee
김민수 <minsukim@gimminsuui-MacBookAir.local>
2024-10-10 20:03:39 +0900

    feat: local 알람 기능

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:prayu_webview/views/home_screen.dart';
+import 'package:prayu_webview/utils/local_notification/notification_service.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-void main() {
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await NotificationService.init();
+  await flutterLocalNotificationsPlugin.getNotificationAppLaunchDetails();
 
   runApp(
     MaterialApp(
