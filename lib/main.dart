fafbaec7ec5d82d7c87763a613e5e236d9bc7e1e
Junbeom Park <ephesi4ns623@gmail.com>
2024-11-04 16:11:02 +0900

    feat: facebook sdk init

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 // main.dart
+import 'package:facebook_app_events/facebook_app_events.dart';
 import 'package:flutter/material.dart';
 import 'package:prayu_webview/services/firebase_sevice.dart';
 import 'package:prayu_webview/views/home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  FacebookAppEvents();
 
   await initFirebaseAndLocalNotifications();
   setupFirebaseMessagingListeners();
