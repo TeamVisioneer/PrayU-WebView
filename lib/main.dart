cea806112507d1270a98877d621cf6bbc31d06e6
Junbeom Park <ephesi4ns623@gmail.com>
2024-11-07 16:53:24 +0900

    fix: kakao sdk 제거

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@
 import 'package:facebook_app_events/facebook_app_events.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 import 'package:prayu_webview/services/firebase_sevice.dart';
 import 'package:prayu_webview/views/home_screen.dart';
 
@@ -10,11 +9,6 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   FacebookAppEvents();
-
-  KakaoSdk.init(
-    nativeAppKey: '57583aa0c3464bf71e902b9c78580e5b',
-    javaScriptAppKey: '8cec546a8802b8b706beb1ffb28b0c8a',
-  );
 
   await Firebase.initializeApp();
   await initFirebaseAndLocalNotifications();
